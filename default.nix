65782456d119e7948974b6e3384859375bfac036
Evan Stoll <evanjsx@gmail.com>
2020-02-10 09:27:26 -0500

    Compile realtime with musl
    The realtime has issues finding some libraries in our minimal environment if it is not compiled with musl
    
    Pass in a musl stdenv to allow it to build with such and properly locate the required libraries

--- default.nix
+++ default.nix
@@ -4,84 +4,86 @@ let
   busybox = pkgs.pkgsStatic.busybox.override {
     extraConfig = lib.readFile ./fixed.config;
   };
+  stdenv = pkgs.pkgsMusl.stdenv;
   nixos = (import (sources.nixpkgs + "/nixos") { configuration = ./nixos.nix; });
   pkgs = (import sources.nixpkgs { overlays = [ overlay ]; });
   lib = pkgs.lib;
   x86_64 = pkgs;
   overlay = self: super: {
-      realtime = (pkgs.callPackage "/home/evanjs/src/rjg/copilot/realtime" {
-        deploy = false;
-        withSensorTester = true;
-        withEthercat = false;
-        softwareVersion = "5.0.0";
-        hardwareVersion = "10.0.0";
-      });
+    realtime = (pkgs.callPackage "/home/evanjs/src/rjg/copilot/realtime" {
+      deploy = false;
+      withSensorTester = true;
+      withEthercat = false;
+      softwareVersion = "5.0.0";
+      hardwareVersion = "10.0.0";
+      inherit stdenv;
+    });
     uart-manager = self.stdenv.mkDerivation {
       name = "uart-manager";
       src = ./uart-manager;
     };
-      script = pkgs.writeTextFile {
-        name = "init";
-        text = ''
+    script = pkgs.writeTextFile {
+      name = "init";
+      text = ''
         #!${self.busybox}/bin/busybox
-        '';
-        executable = true;
-      };
-      initrd-tools = self.buildEnv {
-        name = "initrd-tools";
-        paths = [ self.realtime self.busybox ];
-      };
-      initrd = self.makeInitrd {
-        contents = [
+      '';
+      executable = true;
+    };
+    initrd-tools = self.buildEnv {
+      name = "initrd-tools";
+      paths = [ self.realtime self.busybox ];
+    };
+    initrd = self.makeInitrd {
+      contents = [
         {
           object = "${self.initrd-tools}/bin";
           symlink = "/bin";
-        }
-        {
-            object = self.script;
-            symlink = "/init";
           }
           {
-            object = ./rootfs/etc;
-            symlink = "/etc";
+          object = self.script;
+          symlink = "/init";
           }
           {
-            object = ./rootfs/lib/firmware;
-            symlink = "/lib/firmware";
+          object = ./rootfs/etc;
+          symlink = "/etc";
           }
           {
-            object = "${self.linux}/lib/modules";
-            symlink = "/lib/modules";
+          object = ./rootfs/lib/firmware;
+          symlink = "/lib/firmware";
           }
-        ];
-      };
-      test-script = pkgs.writeShellScript "test-script" ''
+          {
+          object = "${self.linux}/lib/modules";
+          symlink = "/lib/modules";
+          }
+          ];
+          };
+          test-script = pkgs.writeShellScript "test-script" ''
       #!${self.stdenv.shell}
-          ${self.qemu}/bin/qemu-system-x86_64 -kernel ${self.linux}/bzImage -initrd ${self.initrd}/initrd -nographic -m 4096 -append 'console=ttyS0'
+      ${self.qemu}/bin/qemu-system-x86_64 -kernel ${self.linux}/bzImage -initrd ${self.initrd}/initrd -nographic -m 4096 -append 'console=ttyS0'
       '';
-      debug-script = pkgs.writeShellScript "debug-script" ''
+        debug-script = pkgs.writeShellScript "debug-script" ''
       #!${self.stdenv.shell}
-          ${self.qemu}/bin/qemu-system-x86_64 -kernel ${self.linux}/bzImage -initrd ${self.initrd}/initrd -nographic -m 4096 -append 'console=ttyS0 -stdio serial -s -S'
+      ${self.qemu}/bin/qemu-system-x86_64 -kernel ${self.linux}/bzImage -initrd ${self.initrd}/initrd -nographic -m 4096 -append 'console=ttyS0 -stdio serial -s -S'
       '';
-    };
+        };
 
-in pkgs.lib.fix (self: {
-  x86_64 = { inherit (x86_64) debug-script test-script; };
+        in pkgs.lib.fix (self: {
+        x86_64 = { inherit (x86_64) debug-script test-script; };
 
-  kernelShell = nixos.config.boot.kernelPackages.kernel.overrideDerivation
-  (drv: {
-    nativeBuildInputs = drv.nativeBuildInputs
-    ++ (with x86_64; [ ncurses pkgconfig ]);
-    shellHook = ''
+        kernelShell = nixos.config.boot.kernelPackages.kernel.overrideDerivation
+        (drv: {
+        nativeBuildInputs = drv.nativeBuildInputs
+        ++ (with x86_64; [ ncurses pkgconfig ]);
+        shellHook = ''
         addToSearchPath PKG_CONFIG_PATH ${x86_64.ncurses.dev}/lib/pkgconfig
         echo to configure: 'make $makeFlags menuconfig'
         echo to build: 'time make $makeFlags zImage -j8'
-    '';
-  });
-  nixos = {
-    inherit nixos;
-    inherit (nixos) system;
-    inherit (nixos.config.system.build) initialRamdisk;
-    inherit (nixos.config.system.build.kernel) dev;
-  };
-})
+        '';
+      });
+      nixos = {
+        inherit nixos;
+        inherit (nixos) system;
+        inherit (nixos.config.system.build) initialRamdisk;
+        inherit (nixos.config.system.build.kernel) dev;
+      };
+    })
